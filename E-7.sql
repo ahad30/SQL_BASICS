9af92886d55c60b3654df06c25e4bfc8d9d62cd4
ahad30 <mohiminulislamahad@gmail.com>
2025-05-29 22:06:56 +0600

    Problem 7 solved

--- E-7.sql
+++ E-7.sql
@@ -1,3 +1,4 @@
+### 7. Update the Release Year of the Album with no Release Year
 select * from albums
 where release_year is null;
 Update albums
---
+++ E-8.sql
@@ -0,0 +1,3 @@
+### 8. Insert a record for your favorite Band and one of their Albums
+INSERT INTO bands(id,name) VALUES (8,'Arbovirus');
+INSERT INTO albums(id,name,release_year,band_id) VALUES (18,'Tribe of Force',2010,8);
